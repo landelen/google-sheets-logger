c4ccfb7a8479bbc8de5124150d4768c23a21b5bf
Ihor Pulatov <fool@lights-MacBook-Pro.local>
2025-01-10 06:36:30 +0200

    Fixed

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,12 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { emailOptions } from 'config/email.config';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
 
   sendMail(): void {
-    this.mailerService.sendMail(emailOptions());
+    this.mailerService.sendMail(emailOptions(this.configService));
   }
 }
